19d0705ed81c62a65ae33697a5b1c36524244618
Syukur Iman Attaqwa <ataiman30@gmail.com>
2023-03-26 12:26:58 +0700

    set data text language

--- conection_bot.py
+++ conection_bot.py
@@ -1,7 +1,22 @@
 import telebot
+import csv
 
 TOKEN = '6147252599:AAHEXQZNmrKGIjNgkyOMzuEWjw5ObkJtCeo'
 
 def conectToBot():
   tb = telebot.TeleBot(TOKEN)
   return tb
+
+def getDataCsv():
+  data_language_uk = []
+  with open('inggris_language.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    for dataNew in csvreader:
+      data_language_uk.append(dataNew)
+  
+  data_language_id = []
+  with open('indonesia_language.csv', 'r') as file:
+    csvreader = csv.reader(file)
+    for dataNew in csvreader:
+      data_language_id.append(dataNew)
+  return data_language_uk, data_language_id
--- index.py
+++ index.py
@@ -1,18 +1,24 @@
-
 import conection_bot
 import markup_function
 import callback_function
 
+data_language_uk, data_language_id = conection_bot.getDataCsv()
 bot = conection_bot.conectToBot()
+
+print(data_language_id)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-  bot.send_message(call.message.chat.id,  callback_function.check_callback_message(call=call))
+  next = bot.send_message(call.message.chat.id,  callback_function.check_callback_message(call=call))
+  bot.register_next_step_handler(next, main_menu(call.message))
 
 @bot.message_handler(commands=['start', 'help'], func=lambda message: True)
 def send_welcome(message):
   language_markup = markup_function.markup_change_Language()
   bot.send_message(message.chat.id, "Selamat Datang di Bot Magang,\n Pilih Bahasa yang akan digunakan", reply_markup=language_markup)
 
+def main_menu(call):
+  menu_markup = markup_function.markup_change_Language()
+  bot.send_message(call.message.chat.id, "Silahkan pilih menu", reply_markup=menu_markup)
 
 bot.polling()
